5c8b6cc80e97a0eaa78cc344d95e7878e4fae7e6
Jakov <jaki.gudelj@yahoo.de>
2020-06-21 15:08:12 +0200

    a

--- Aufgabe_08/server.js
+++ Aufgabe_08/server.js
@@ -1,2 +1,19 @@
 "use strict";
+var Aufgabe08;
+(function (Aufgabe08) {
+    let button = document.getElementById("senden");
+    button.addEventListener("click", buttonclk);
+    function buttonclk(_event) {
+        let formData = new FormData(document.forms[0]);
+        let url = "https://jakovgudeljgis.herokuapp.com/";
+        let query = new URLSearchParams(formData);
+        url = url + "?" + query.toString();
+        addUrl(url);
+    }
+    async function addUrl(_url) {
+        let response = await fetch(_url, { method: "get" });
+        let responsestring = await response.text();
+        console.log("Antwort: " + responsestring);
+    }
+})(Aufgabe08 || (Aufgabe08 = {}));
 //# sourceMappingURL=server.js.map
