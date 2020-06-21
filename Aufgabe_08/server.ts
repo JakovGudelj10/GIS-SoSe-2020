f08eff2ec2f330b34001c5f4bb72db9310d7d2f8
Jakov <jaki.gudelj@yahoo.de>
2020-06-21 15:13:08 +0200

    a

--- Aufgabe_08/server.ts
+++ Aufgabe_08/server.ts
@@ -5,7 +5,7 @@ namespace Aufgabe08 {
 
     function buttonclk(_event: Event): void { 
         let formData: FormData = new FormData(document.forms[0]);
-        let url: string = "https://jakovgudeljgis.herokuapp.com/";
+        let url: string = "https://gissose2020gudeljja.herokuapp.com/";
         let query: URLSearchParams = new URLSearchParams(<any>formData);
         url = url + "?" + query.toString();
         addUrl(url);
