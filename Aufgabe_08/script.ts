9636d82a89e789dcce597fd1cade01f2a38804c7
Jakov <jaki.gudelj@yahoo.de>
2020-06-24 12:18:54 +0200

    Aufgabe 8

---
+++ Aufgabe_08/script.ts
@@ -0,0 +1,21 @@
+namespace Aufgabe08 {
+
+    let button: HTMLButtonElement = document.getElementById("senden") as HTMLButtonElement;
+    button.addEventListener("click", buttonclk);
+
+    function buttonclk(_event: Event): void { 
+        let formData: FormData = new FormData(document.forms[0]);
+        let url: string = "https://gissose2020gudeljja.herokuapp.com";
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        url = url + "?" + query.toString();
+        addUrl(url);
+    
+    }
+    async function addUrl(_url: RequestInfo): Promise<void> {
+        let response: Response = await fetch(_url, { method: "get" });
+        let responsestring: String = await response.text();
+        console.log("Antwort: " + responsestring);
+      }
+
+
+}
--- Aufgabe_08/server.js
+++ Aufgabe_08/server.js
@@ -1,19 +1,27 @@
 "use strict";
-var Aufgabe08;
-(function (Aufgabe08) {
-    let button = document.getElementById("senden");
-    button.addEventListener("click", buttonclk);
-    function buttonclk(_event) {
-        let formData = new FormData(document.forms[0]);
-        let url = "https://gissose2020gudeljja.herokuapp.com/";
-        let query = new URLSearchParams(formData);
-        url = url + "?" + query.toString();
-        addUrl(url);
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.A08Server = void 0;
+const Http = require("http");
+var A08Server;
+(function (A08Server) {
+    console.log("Starting server");
+    let port = Number(process.env.PORT);
+    if (!port)
+        port = 8100;
+    let server = Http.createServer();
+    server.addListener("request", handleRequest);
+    server.addListener("listening", handleListen);
+    server.listen(port);
+    function handleListen() {
+        console.log("Listening");
     }
-    async function addUrl(_url) {
-        let response = await fetch(_url, { method: "get" });
-        let responsestring = await response.text();
-        console.log("Antwort: " + responsestring);
+    function handleRequest(_request, _response) {
+        console.log("I hear voices!");
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        _response.write(_request.url);
+        console.log(_request.url);
+        _response.end();
     }
-})(Aufgabe08 || (Aufgabe08 = {}));
+})(A08Server = exports.A08Server || (exports.A08Server = {}));
 //# sourceMappingURL=server.js.map
